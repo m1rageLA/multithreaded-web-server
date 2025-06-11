0ea95edcd17be82b85cb6865269d252174180911
m1rage <miragela.code@gmail.com>
2025-06-11 18:21:40 +0200

    Add message output via send method

--- src/__core__/response.rs
+++ src/__core__/response.rs
@@ -1,4 +1,4 @@
-use std::net::TcpStream;
+use std::{io::Write, net::TcpStream};
 
 use crate::__core__::types::{Request, Response};
 
@@ -12,6 +12,9 @@ impl<'a> Response<'a> {
         }
     }
     pub fn send(&mut self, message: &str) {
+        //format can bundle the message
+        let response = format!("HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n{}", message);
+        self.stream.write_all(response.as_bytes()).unwrap();
         println!("{}", message);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use __core__::{App};
 fn main() {
     let mut app = App::new("127.0.0.1:7878".to_string());
 
-
     app.get("", |req, mut res| {
         res.send("Hello world from my first method!");
         Err("Error".into())
